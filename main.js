a21f5183cfb9bc2af89ca0838c093ba826d77d7f
Alan Mendes <alan.raion@gmail.com>
2024-12-15 16:19:58 -0300

    Exercicio HTML com JS, validação.

---
+++ main.js
@@ -0,0 +1,31 @@
+const valorA = document.getElementById('campoA');
+const valorB = document.getElementById('campoB');
+const form = document.getElementById('validar');
+
+function validar(numero){
+    numero = valorA.value > valorB.value;
+    return numero;
+}
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const mensagemResposta = document.getElementById("mensagem");
+        const mensagemA = 'O valor do campo A é maior que o valor do campo B' 
+        const mensagemB = 'O valor do campo B é maior que o valor do campo A'
+        const mensagemIguais = 'Os valor dos campor são Iguais'
+
+        if (valorB.value > valorA.value){
+            mensagemResposta.innerHTML= mensagemB;
+            mensagemResposta.style.display = "block";
+        }
+        else if (valorB.value == valorA.value){
+            mensagemResposta.innerHTML= mensagemIguais;
+            mensagemResposta.style.display = "block";
+        }
+        else {
+            mensagemResposta.innerHTML= mensagemA;
+            mensagemResposta.style.display = "block";
+        }
+
+})
